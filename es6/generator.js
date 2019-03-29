fd4ea5293e2027509666822ee4994c3b38c7f099
niuben <741001695@qq.com>
2019-03-29 16:07:21 +0800

    生成器多函数执行

--- es6/generator.js
+++ es6/generator.js
@@ -25,12 +25,12 @@ function run (taskDef) {
             }          
             console.log("if", data);
             result = task.next(data);
-            step()
+            step();
         })
     } else {
         console.log("else", result.value);
-        result = task.next(result.value)
-        step()
+        result = task.next(result.value);
+        step();
       }
     }
   }
@@ -38,9 +38,71 @@ function run (taskDef) {
   step()
 }
 
-run(function * () {
-    let contents = yield readFile('./generator.html');
-    //   doSomethingWith(contents)
-    console.log(contents);
-    console.log('Done')
-})
+
+// var createIterator = function * () {
+//     let contents = yield readFile('./demo/es6/generator.html');
+//     console.log("contents", contents);
+//     // console.log('Done');
+// }
+//     // run(createIterator);
+    
+// run(createIterator);
+
+// 异步编程
+var readFile1 = function(name){
+    return function(callback){
+        var duration = name == "content1" ? 2000 : 1000;                
+        setTimeout(function(){
+            console.log("name", name, "duration", duration);                                            
+            callback && callback(duration);
+        }, duration);
+    }
+}
+
+var createIterator1 = function*(){    
+    var content1 = yield readFile1("content1");
+    var content2 = yield readFile1("content2");
+    console.log(content1);
+    console.log(content2);
+}
+
+function runTask(func){
+    var task = func();
+    var result = task.next();
+        
+    function step(){
+
+        if(result.done == true){
+            return;
+        }
+
+        if(typeof result.value == "function"){
+            result.value(function(data){
+                result = task.next(data);
+                step();
+            });
+            
+        }else{
+            result = task.next(data);  
+            step();
+        }        
+    }
+    step();
+}
+
+// var iterator1 = createIterator1();
+// console.log(iterator1.next());
+runTask(createIterator1);
+
+
+//生成器传值
+// var createIterator2 = function*(){
+//     var first = yield 1;
+//     var second = yield first + 2;
+//     var third = yield second + 3;
+// }
+
+// var iterator2 = createIterator2();
+// console.log(iterator2.next());
+// console.log(iterator2.next(2)); //4
+// console.log(iterator2.next(3)); //6
